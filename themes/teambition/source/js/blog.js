8d1afef6c572ad7673fc1e1ff24b81e8dd2ea1d8
wanghonglai <wanghonglai@touweiqiangs-Mac-mini.local>
2013-09-02 13:26:30 +0800

    fix date flow problem

--- themes/teambition/source/js/blog.js
+++ themes/teambition/source/js/blog.js
@@ -1,3 +1,19 @@
+//side bar
 $('#switcher-toggler').click(function(){
   document.body.className=(document.body.className==''?'show-switcher':'');
 });
+
+//date flow
+
+arts=$('#post>article');
+var left=0,right=0;
+for(var i=0;i<arts.length;i++){
+  if(left<=right){
+    arts[i].className='left';
+    left+=arts[i].offsetHeight;
+  }
+  else{
+    arts[i].className='right';
+    right+=arts[i].offsetHeight;
+  }
+}
