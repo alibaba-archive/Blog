b4bfa8deec312a2deb3023020652cb1930fbfa48
waksana <waksana@163.com>
2013-09-23 14:12:19 +0800

    improve the design

--- themes/teambition/source/js/blog.js
+++ themes/teambition/source/js/blog.js
@@ -1,10 +1,4 @@
-//side bar
-$('#switcher-toggler').click(function(){
-  document.body.className=(document.body.className==''?'show-switcher':'');
-});
-
 //date flow
-
 arts=$('#post>article');
 var left=0,right=0;
 for(var i=0;i<arts.length;i++){
@@ -17,3 +11,24 @@ for(var i=0;i<arts.length;i++){
     right+=arts[i].offsetHeight;
   }
 }
+title=$('.brand');
+function navHeight(top,h){
+  var rate=1.*top/h;
+  var height=(70-h*rate)+'px';
+  var bg=(-15-h*2/5*rate)+'px';
+  bg='15px '+bg;
+  title.css({
+    'height':height,
+    'line-height':height,
+    'background-position':bg
+  });
+}
+function navControl(){
+  var h=45;
+  var top=$(window).scrollTop();
+  if(top>h)return navHeight(h,h);
+  if(top<0)return navHeight(0,h);
+  navHeight(top,h);
+}
+navHeight(0,45);
+$(window).scroll(navControl);
